e29640780a19361fb73098eff893b5ef58ad10dd
iv-thinkpad <gewooniv@gmail.com>
2024-05-23 09:04:10 +0200

    added template setup

--- articles/views.py
+++ articles/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
-    return None
+    return render(request, 'articles/index.html')
 
 def posts(request):
     pass
