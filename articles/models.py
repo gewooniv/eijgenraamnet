552edf733c5e2caa798985ac3b004ef6393350a2
iv-thinkpad <gewooniv@gmail.com>
2024-05-25 17:28:40 +0200

    added dummy data

--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
     title = models.CharField(max_length=64)
     author = models.CharField(max_length=64)
     date = models.DateField()
-    img = models.ImageField()
     excerpt = models.CharField(max_length=200)
     content = models.CharField(max_length=2000)
+    
+    def __str__(self):
+        return f'{self.title}, van {self.author} ({self.date})'
