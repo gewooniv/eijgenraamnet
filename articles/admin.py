78005fece3f8e6566fe3b55ce3931bee4508d500
iv-thinkpad <gewooniv@gmail.com>
2024-05-29 14:27:40 +0200

    added author model

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Author
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ class PostAdmin(admin.ModelAdmin):
     # list_filter = ('author', )
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
--- articles/models.py
+++ articles/models.py
@@ -4,10 +4,19 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(m.Model):
+    first_name = m.CharField(max_length=128)
+    last_name = m.CharField(max_length=128)
+    dob = m.DateField()
+    
+    def __str__(self):
+        return f'{self.first_name}, {self.dob}'
+    
 class Post(m.Model):
     title = m.CharField(max_length=64)
     slug = m.SlugField(default='', blank=True, null=False, db_index=True)
     date = m.DateField()
+    author = m.ForeignKey(Author, on_delete=m.SET_NULL, null=True)
     excerpt = m.TextField(max_length=200)
     content = m.TextField(max_length=20000)
     
