ba2b0d2041df2a37b776911feaa1e71cfbb1570c
iv-thinkpad <gewooniv@gmail.com>
2024-06-14 16:01:47 +0200

    added picture class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models as m
 
 # Create your models here.
+
+class Picture(m.Model):
+    name = m.CharField(max_length=255)
+    image = m.ImageField(upload_to='gallery/') 
+    description = m.TextField()
+    date = m.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}, {self.date}'
