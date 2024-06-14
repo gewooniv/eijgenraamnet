976cf4443c33eec656e25b5e532e1341bd2537ff
iv-thinkpad <gewooniv@gmail.com>
2024-06-14 15:08:03 +0200

    added gallery index

--- eijgenraamnet/urls.py
+++ eijgenraamnet/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('articles.urls'))
+    path('', include('articles.urls')),
+    path('', include('gallery.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
