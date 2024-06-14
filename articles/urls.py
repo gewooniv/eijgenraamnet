e7ebbccb9b6bd83c318916c692da13878ad29d9b
iv-thinkpad <gewooniv@gmail.com>
2024-06-14 11:38:18 +0200

    moved +static from urlpatterns

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,4 @@
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 
@@ -10,4 +8,4 @@ urlpatterns = [
     path('posts', views.PostsView.as_view(), name='posts'),
     path('posts/<slug:slug>', views.PostView.as_view(), name='post-page'),
     path('ckeditor/', include('ckeditor_uploader.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
