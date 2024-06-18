81bd2f560503d7b831ec764ab9a26eafc01b82d0
iv-thinkpad <gewooniv@gmail.com>
2024-06-18 12:48:21 +0200

    rewritten has_responses

--- guestbook/templatetags/commenttags.py
+++ guestbook/templatetags/commenttags.py
@@ -1,13 +1,11 @@
 from django import template
-from guestbook.models import Comment
 
 register = template.Library()
 
 @register.filter
 def in_comment(all_responses, comment):
-
     return all_responses.filter(comment=comment)
 
 @register.filter
-def has_response(comment):
-    return bool(Comment.objects.filter(comment=comment))
+def has_response(comment, all_responses):
+    return bool(all_responses.filter(comment=comment))
