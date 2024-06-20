2545ced2dd76d1d416c309128027abed74ca80b2
iv-thinkpad <gewooniv@gmail.com>
2024-06-20 14:10:59 +0200

    added custom user admin registration

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Author
+from django.contrib.auth.admin import UserAdmin
+from .models import Post, Author, CustomUser
 
 # Register your models here.
 
@@ -8,6 +9,56 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     # list_display
     list_filter = ('author', )
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "first_name",
+        "last_name",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": (
+            "first_name",
+            "last_name", 
+            "email", 
+            "password", 
+            "date_of_birth")}
+        ),
+        ("Permissions", {"fields": (
+            "is_staff", 
+            "is_active", 
+            "groups", 
+            "user_permissions")}
+        ),
+    )
+    add_fieldsets = (
+        ( None, {"fields": (
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+            "date_of_birth",
+            "is_staff",
+            "is_active",
+            "groups",
+            "user_permissions")}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
