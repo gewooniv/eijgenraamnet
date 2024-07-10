dd67163b9c970e879d232f878b927d2abacbe633
iv-thinkpad <gewooniv@gmail.com>
2024-07-10 14:02:35 +0200

    redo collectstatic

--- eijgenraamnet/settings.py
+++ eijgenraamnet/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+rldtd9^7aigu88fq17_kd*dzm6#4=luhgag5w!thz4jn75$mq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -125,8 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
-#STATIC_ROOT = BASE_DIR/'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
