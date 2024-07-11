92cb83bc840763ffe5aed59e71efc2b06e081ac6
iv-thinkpad <gewooniv@gmail.com>
2024-07-11 10:07:41 +0200

    added eb settings

--- eijgenraamnet/settings.py
+++ eijgenraamnet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+rldtd9^7aigu88fq17_kd*dzm6#4=luhgag5w!thz4jn75$mq'
+SECRET_KEY = 'django-insecure-+rldtd9^7aigu88fq17_kd*dzm6#4=luhgag5w!thz4jn75$mq' # getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = getenv("IS_DEVELOPMENT", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
